862b7789200aaa24e1c60ac6358290cdf3c451d3
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-02-12 15:19:40 +0300

    Добавил сервер

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthComponent } from './auth/auth.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, AuthComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+})
