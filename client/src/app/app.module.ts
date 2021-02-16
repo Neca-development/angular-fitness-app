852c2cca6781ab268d0cb54f22d4923469298c39
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-02-16 03:25:55 +0300

    Сделал логин через token

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthComponent } from './auth/auth.component';
 import { FormsModule } from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent],
+  declarations: [AppComponent, AuthComponent, UserComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
